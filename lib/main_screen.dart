9357cd4e8b2a33733681c5b02d39b3b669b61946
Ujjval Joshi <ujjvaljoshi45@gmail.com>
2023-06-05 14:15:55 +0530

    done

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -5,6 +5,11 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main Screen"),
+      ),
+      body: const Center(child: Text('hello this is main Screen')),
+    );
   }
 }
